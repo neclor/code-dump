3509115258d0c27159f120bb6ffcdf06d3063d0b
neclor <neclordev@gmail.com>
2025-11-28 16:26:01 +0100

    0.1 solution done

--- python/logic-project/src/solution_template.py
+++ python/logic-project/src/solution_template.py
@@ -11,7 +11,15 @@ def question1(M: int, N: int, i0: int, j0: int) -> tuple[list[list[int]], Glucos
 
 
 def question3() -> int:
-    return len(ChessBoard(3, 4).get_all_solutions())
+    height: int = 3
+    width: int = 4
+
+    solution_count: int = 0
+    for row in range(height):
+        for column in range(width):
+            solution_count += len(ChessBoard(height, width, row, column).get_all_solutions())
+
+    return solution_count
 
 
 def question4() -> int:
@@ -51,8 +59,13 @@ def question4() -> int:
         return False
 
 
-    board: ChessBoard = ChessBoard(3, 4)
-    solutions: list[list[list[int]]] = board.get_all_matrix_solutions()
+    height: int = 3
+    width: int = 4
+
+    solutions: list[list[list[int]]] = []
+    for row in range(height):
+        for column in range(width):
+            solutions += ChessBoard(height, width, row, column).get_all_matrix_solutions()
 
     i: int = 0
     while (i < len(solutions) - 1):
@@ -89,45 +102,34 @@ class ChessBoard:
     KNIGHT_MOVES: list[tuple[int, int]] = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
 
 
-    def __init__(self, height: int, width: int, start_row: int | None = None, start_column: int | None = None) -> None:
+    def __init__(self, height: int, width: int, start_row: int = 0, start_column: int = 0) -> None:
         if height <= 0: raise ValueError("Height must be positive.")
         if width <= 0: raise ValueError("Width must be positive.")
+        if not (0 <= start_row < height): raise ValueError("Starting row out of bounds.")
+        if not (0 <= start_column < width): raise ValueError("Starting column out of bounds.")
 
         self.height: int = height
         self.width: int = width
+        self.start_row: int = start_row
+        self.start_column: int = start_column
+
         self.cell_count: int = height * width
+        self.black_cell_count: int = self.cell_count // 2
+        self.white_cell_count: int = self.cell_count - self.black_cell_count
+
+        self.start_cell_color: int = (start_row + start_column) % 2 # 0 - white, 1 - black
+
+        self.var_count_for_even_turn: int = 0
+        self.var_count_for_odd_turn: int = 0
+        if self.start_cell_color == 1:
+            self.var_count_for_even_turn = self.black_cell_count - 1
+            self.var_count_for_odd_turn = self.white_cell_count
+        else:
+            self.var_count_for_even_turn = self.white_cell_count - 1
+            self.var_count_for_odd_turn = self.black_cell_count
+
         self.solver: Glucose3 = Glucose3()
-
-        if start_row is not None and start_column is not None:
-            self.set_start_position(start_row, start_column)
         self._add_constraints()
-
-
-    def reset_solver(self) -> None:
-        self.solver = Glucose3()
-        self._add_constraints()
-
-
-    def set_start_position(self, start_row: int = 0, start_column: int = 0) -> None:
-        if not (0 <= start_row < self.height): raise ValueError("Starting row out of bounds.")
-        if not (0 <= start_column < self.width): raise ValueError("Starting column out of bounds.")
-        self.solver.add_clause([self.var_c(start_row, start_column, 0)])
-
-
-    # Variable by coords
-    def var_c(self, row: int, column: int, turn: int) -> int:
-        if not (0 <= row < self.height and 0 <= column < self.width): raise ValueError("Row or column out of bounds.")
-        return self.var_i(row * self.width + column, turn)
-
-
-    # Variable by index
-    def var_i(self, index: int, turn: int) -> int:
-        if not (0 <= index < self.cell_count and 0 <= turn < self.cell_count): raise ValueError("Index or turn out of bounds.")
-        return turn * self.cell_count + index + 1
-
-
-    def get_variables(self) -> list[int]:
-        return [self.var_i(i, turn) for i in range(self.cell_count) for turn in range(self.cell_count)]
 
 
     def get_solution(self) -> list[tuple[int, int]]:
@@ -155,11 +157,122 @@ class ChessBoard:
         return [self._list_to_matrix(solution) for solution in self.get_all_solutions()]
 
 
+    def get_variables(self) -> list[int]:
+        return [self.var_i(i, turn) for turn in range(1, self.cell_count) for i in range(self._get_turn_var_count(turn))]
+
+
+    # Variable by coords
+    def var_c(self, row: int, column: int, turn: int) -> int:
+        if not (0 <= row < self.height and 0 <= column < self.width): raise ValueError("Row or column out of bounds.")
+        if self._is_start_cell(row, column): raise ValueError("No variable for the starting position.")
+        if self._get_cell_color(row, column) != self._get_turn_color(turn): raise ValueError("No variable for this cell at this turn.")
+
+        index: int = (row * self.width + column) // 2
+        if (self.start_cell_color == self._get_turn_color(turn)) and ((self.start_row < row) or (self.start_row == row and self.start_column < column)):
+            index -= 1
+
+        return self.var_i(index, turn)
+
+
+    # Variable by index
+    def var_i(self, index: int, turn: int) -> int:
+        if not (0 < turn < self.cell_count): raise ValueError("Turn out of bounds.")
+        if not (0 <= index < self._get_turn_var_count(turn)): raise ValueError("Index out of bounds.")
+        return self.var_count_for_odd_turn * (turn // 2) + self.var_count_for_even_turn * ((turn - 1) // 2) + index + 1
+
+
+    def _get_turn_var_count(self, turn: int) -> int:
+        return self.var_count_for_even_turn if turn % 2 == 0 else self.var_count_for_odd_turn
+
+
+    def _get_start_turn(self, row: int, column: int) -> int:
+        return 2 if self.start_cell_color == self._get_cell_color(row, column) else 1
+
+
+    def _get_cell_color(self, row: int, column: int) -> int:
+        return (row + column) % 2
+
+
+    def _get_turn_color(self, turn: int) -> int:
+        return (self.start_cell_color + turn) % 2
+
+
+    def _is_start_cell(self, row: int, column: int) -> bool:
+        return row == self.start_row and column == self.start_column
+
+
+    def _add_constraints(self) -> None:
+        self._add_knight_move_constraints()
+        self._add_turn_constraints()
+        self._add_cell_constraints()
+
+
+    # At each turn, exactly one cell is visited
+    def _add_turn_constraints(self) -> None:
+        for turn in range(1, self.cell_count):
+            var_count: int = self._get_turn_var_count(turn)
+            self.solver.add_clause([self.var_i(i, turn) for i in range(var_count)]) # At least one cell is visited
+
+            for i in range(var_count - 1):
+                for j in range(i + 1, var_count):
+                    self.solver.add_clause([-self.var_i(i, turn), -self.var_i(j, turn)]) # At most one cell is visited
+
+
+    # Each cell is visited exactly once
+    def _add_cell_constraints(self) -> None:
+        for row in range(self.height):
+            for column in range(self.width):
+                if self._is_start_cell(row, column): continue
+
+                start_turn: int = self._get_start_turn(row, column)
+                self.solver.add_clause([self.var_c(row, column, turn) for turn in range(start_turn, self.cell_count, 2)]) # Cell is visited at least once
+
+                for turn_1 in range(start_turn, self.cell_count - 2, 2):
+                    for turn_2 in range(turn_1 + 2, self.cell_count, 2):
+                        self.solver.add_clause([-self.var_c(row, column, turn_1), -self.var_c(row, column, turn_2)]) # Cell is visited at most once
+
+
+    # Valid knight moves
+    def _add_knight_move_constraints(self) -> None:
+        for row in range(self.height):
+            for column in range(self.width):
+
+                start_turn: int = 0
+                cell_count: int = 0
+                if self._is_start_cell(row, column):
+                    start_turn = 0
+                    cell_count = 1
+                else:
+                    start_turn = self._get_start_turn(row, column)
+                    cell_count = self.cell_count - 1
+
+                for turn in range(start_turn, cell_count, 2):
+                    next_positions: list[int] = []
+                    for delta_row, delta_column in self.KNIGHT_MOVES:
+                        next_row: int = row + delta_row
+                        next_column: int = column + delta_column
+                        if (
+                            (not (0 <= next_row < self.height and 0 <= next_column < self.width)) or
+                            (next_row == self.start_row and next_column == self.start_column)
+                        ): continue
+                        next_positions.append(self.var_c(next_row, next_column, turn + 1))
+
+                    if self._is_start_cell(row, column):
+                        self.solver.add_clause(next_positions)
+                    else:
+                        self.solver.add_clause([-self.var_c(row, column, turn)] + next_positions)
+
+
     def _model_to_solution(self, model: list[int]) -> list[tuple[int, int]]:
-        solution: list[tuple[int, int]] = []
-        for turn in range(self.cell_count):
+        solution: list[tuple[int, int]] = [(self.start_row, self.start_column)]
+        for turn in range(1, self.cell_count):
             for row in range(self.height):
                 for column in range(self.width):
+                    if (
+                        self._get_cell_color(row, column) != self._get_turn_color(turn) or
+                        self._is_start_cell(row, column)
+                    ): continue
+
                     if model[self.var_c(row, column, turn) - 1] > 0:
                         solution.append((row, column))
         return solution
@@ -172,57 +285,36 @@ class ChessBoard:
         return matrix_solution
 
 
-    def _add_constraints(self) -> None:
-        self._add_turn_constraints()
-        self._add_knight_move_constraints()
-        self._add_cell_constraints()
-
-
-    # At each turn, exactly one cell is visited
-    def _add_turn_constraints(self) -> None:
-        for turn in range(self.cell_count):
-            self.solver.add_clause([self.var_i(i, turn) for i in range(self.cell_count)]) # At least one cell is visited
-
-            for i in range(self.cell_count - 1):
-                for j in range(i + 1, self.cell_count):
-                    self.solver.add_clause([-self.var_i(i, turn), -self.var_i(j, turn)]) # At most one cell is visited
-
-
-    # Each cell is visited exactly once
-    def _add_cell_constraints(self) -> None:
-        for i in range(self.cell_count):
-            self.solver.add_clause([self.var_i(i, turn) for turn in range(self.cell_count)]) # Cell is visited at least once
-
-            for turn_1 in range(self.cell_count - 1):
-                for turn_2 in range(turn_1 + 1, self.cell_count):
-                    self.solver.add_clause([-self.var_i(i, turn_1), -self.var_i(i, turn_2)]) # Cell is visited at most once
-
-
-    # Valid knight moves
-    def _add_knight_move_constraints(self) -> None:
-        for turn in range(self.cell_count - 1):
-            for row in range(self.height):
-                for column in range(self.width):
-
-                    next_positions: list[int] = []
-                    for delta_row, delta_column in self.KNIGHT_MOVES:
-                        next_row: int = row + delta_row
-                        next_column: int = column + delta_column
-                        if not (0 <= next_row < self.height and 0 <= next_column < self.width): continue
-                        next_positions.append(self.var_c(next_row, next_column, turn + 1))
-
-                    self.solver.add_clause([-self.var_c(row, column, turn)] + next_positions)
-
-
 def main() -> None:
     import time
+    board: ChessBoard = ChessBoard(8, 8)
+
     start_time = time.time()
-    board: ChessBoard = ChessBoard(7, 5, 0, 0)
     board.solver.solve()
-    print(len(board.get_all_solutions()))
     end_time = time.time()
+
     execution_time = end_time - start_time
     print(f"{execution_time:.4f}s")
 
+
+def var_test() -> None:
+    board = ChessBoard(4, 5, 2, 2)
+    for turn in range(1, board.cell_count):
+        matrix: list[list] = []
+        for row in range(board.height):
+            matrix_row: list = []
+            for column in range(board.width):
+                if board._get_cell_color(row, column) != board._get_turn_color(turn):
+                    matrix_row.append("_")
+                elif board._is_start_cell(row, column):
+                    matrix_row.append("s")
+                else:
+                    matrix_row.append(str(board.var_c(row, column, turn)))
+
+            matrix.append(matrix_row)
+
+        for line in matrix:
+            print(line)
+        print()
 
 if __name__ == '__main__': main()

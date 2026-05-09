c872fc7389dcec577feb93e4e8d7c830ba5be249
neclor <neclordev@gmail.com>
2026-05-10 00:04:06 +0200

    abc

--- c/univ/sda-project-2/src/BST/BST.c
+++ c/univ/sda-project-2/src/BST/BST.c
@@ -1,30 +1,30 @@
-%:include <stdio.h>
-%:include <stdlib.h>
-%:include <stddef.h>
-%:include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-%:include "BST.h"
-%:include "List.h"
+#include "BST.h"
+#include "List.h"
 
-typedef struct kvpair_t <%
+typedef struct kvpair_t {
 	void *key;
 	void *value;
-%> KVpair;
+} KVpair;
 
 typedef struct BNode_t BNode;
-struct BNode_t <%
+struct BNode_t {
 	BNode *parent;
 	BNode *left;
 	BNode *right;
 	void *key;
 	void *value;
-%>;
+};
 
-struct BST_t <%
+struct BST_t {
 	BNode *root;
 	size_t size;
 	int (*compfn)(void *, void *);
-%>;
+};
 
 /* ======= Prototypes of static functions ======= */
 
@@ -40,87 +40,80 @@ static void depthRecursive(BNode *node, size_t depth, size_t *sum, size_t *n);
 
 /* ======= Public functions ======= */
 
-BST *bstNew(int comparison_fn_t(void *, void *)) <%
+BST *bstNew(int comparison_fn_t(void *, void *)) {
 	BST *bst = malloc(sizeof(BST));
 	if (bst == NULL)
-	<%
+	{
 		fprintf(stderr, "bestNew: allocation error");
 		exit(1);
-	%>
+	}
 	bst->root = NULL;
 	bst->size = 0;
 	bst->compfn = comparison_fn_t;
 	return bst;
-%>
+}
 
-void bstFree(BST *bst, bool freeKey, bool freeValue) <%
+void bstFree(BST *bst, bool freeKey, bool freeValue) {
 	bstFreeRec(bst->root, freeKey, freeValue);
 	free(bst);
-%>
+}
 
-size_t bstSize(BST *bst) <%
+size_t bstSize(BST *bst) {
 	return bst->size;
-%>
+}
 
-size_t bstHeight(BST *bst) <%
+size_t bstHeight(BST *bst) {
 	if (!bst->root) return 0;
 	return bstHeightRec(bst->root) - 1;
-%>
+}
 
-bool bstInsert(BST *bst, void *key, void *value) <%
-	if (bst->root == NULL) <%
+bool bstInsert(BST *bst, void *key, void *value) {
+	if (bst->root == NULL) {
 		bst->root = bnNew(key, value);
-		if (bst->root == NULL)
-		<%
-			return false;
-		%>
 		bst->size++;
 		return true;
-	%>
+	}
 	BNode *prev = NULL;
 	BNode *n = bst->root;
-	while (n != NULL) <%
+	while (n != NULL) {
 		prev = n;
 		int cmp = bst->compfn(key, n->key);
 		if (cmp <= 0)
-		<%
+		{
 			n = n->left;
-		%>
+		}
 		else if (cmp > 0)
-		<%
+		{
 			n = n->right;
-		%>
-	%>
+		}
+	}
 	BNode *new = bnNew(key, value);
-	if (new == NULL) <%
-		return false;
-	%>
 	new->parent = prev;
-	if (bst->compfn(key, prev->key) <= 0) <%
+	if (bst->compfn(key, prev->key) <= 0) {
 		prev->left = new;
-	%> else <%
+	} else {
 		prev->right = new;
-	%>
+	}
 	bst->size++;
 	return true;
-%>
+}
 
-void *bstSearch(BST *bst, void *key) <%
+void *bstSearch(BST *bst, void *key) {
 	BNode *n = bst->root;
-	while (n != NULL) <%
+	while (n != NULL) {
 		int cmp = bst->compfn(key, n->key);
-		if (cmp < 0) <%
+		if (cmp < 0) {
 			n = n->left;
-		%> else if (cmp > 0) <%
+		} else if (cmp > 0) {
 			n = n->right;
-		%> else <%
+		} else {
 			return n->value;
-		%>
-	%>
+		}
+	}
 	return NULL;
-%>
+}
 
-BST *bstOptimalBuild(int comparison_fn_t(void *, void *), List *lkeys, List *lvalues) <%
+BST *bstOptimalBuild(int comparison_fn_t(void *, void *), List *lkeys, List *lvalues) {
 	if (!comparison_fn_t || !lkeys || !lvalues) return NULL;
 
 	BST *bst = bstNew(comparison_fn_t);
@@ -131,9 +124,9 @@ BST *bstOptimalBuild(int comparison_fn_t(void *, void *), List *lkeys, List *lva
 	free(pairs);
 
 	return bst;
-%>
+}
 
-List *bstRangeSearch(BST *bst, void *keymin, void *keymax) <%
+List *bstRangeSearch(BST *bst, void *keymin, void *keymax) {
 	if (!bst) return NULL;
 
 	List *list = listNew();
@@ -141,9 +134,9 @@ List *bstRangeSearch(BST *bst, void *keymin, void *keymax) <%
 
 	bstRangeSearchRecursive(bst->compfn, bst->root, keymin, keymax, list);
 	return list;
-%>
+}
 
-double bstAverageNodeDepth(BST *bst) <%
+double bstAverageNodeDepth(BST *bst) {
 	if (!bst || !bst->root) return 0;
 
 	size_t sum = 0;
@@ -152,11 +145,11 @@ double bstAverageNodeDepth(BST *bst) <%
 	if (n == 0) return 0;
 
 	return (double)sum / (double)n;
-%>
+}
 
 /* ======= Static functions ======= */
 
-static void bstFreeRec(BNode *n, bool freeKey, bool freeValue) <%
+static void bstFreeRec(BNode *n, bool freeKey, bool freeValue) {
 	if (n == NULL)
 		return;
 	bstFreeRec(n->left, freeKey, freeValue);
@@ -166,23 +159,23 @@ static void bstFreeRec(BNode *n, bool freeKey, bool freeValue) <%
 	if (freeValue)
 		free(n->value);
 	free(n);
-%>
+}
 
-static BNode *bnNew(void *key, void *value) <%
+static BNode *bnNew(void *key, void *value) {
 	BNode *n = malloc(sizeof(BNode));
-	if (n == NULL) <%
+	if (n == NULL) {
 		fprintf(stderr, "bnNew: allocation error\n");
 		exit(1);
-	%>
+	}
 	n->parent = NULL;
 	n->left = NULL;
 	n->right = NULL;
 	n->key = key;
 	n->value = value;
 	return n;
-%>
+}
 
-static size_t bstHeightRec(BNode *root) <%
+static size_t bstHeightRec(BNode *root) {
 	if (!root)
 		return 0;
 
@@ -192,96 +185,96 @@ static size_t bstHeightRec(BNode *root) <%
 		return 1 + hleft;
 	else
 		return 1 + hright;
-%>
+}
 
 /* ======= My static functions ======= */
 
-static KVpair *bstListsToPairs(List *keys, List *values) <%
+static KVpair *bstListsToPairs(List *keys, List *values) {
 	KVpair *array = malloc(keys->size * sizeof(KVpair));
-	if (!array) <%
+	if (!array) {
 		fprintf(stderr, "bstListToArray: allocation error");
 		exit(1);
-	%>
+	}
 
 	LNode *keyNode = keys->head;
 	LNode *valueNode = values->head;
 
-	for (size_t i = 0; keyNode && valueNode; i++) <%
-		array<:i:>.key = keyNode->value;
-		array<:i:>.value = valueNode->value;
+	for (size_t i = 0; keyNode && valueNode; i++) {
+		array[i].key = keyNode->value;
+		array[i].value = valueNode->value;
 
 		keyNode = keyNode->next;
 		valueNode = valueNode->next;
-	%>
+	}
 
 	return array;
-%>
+}
 
-static void bstQuickSort(int comparison_fn_t(void *, void *), KVpair *pairs, size_t left, size_t right) <%
+static void bstQuickSort(int comparison_fn_t(void *, void *), KVpair *pairs, size_t left, size_t right) {
 	if (right - left < 2) return;
 
-	void *pivot = pairs<:left + (right - left) / 2:>.key;
+	void *pivot = pairs[left + (right - left) / 2].key;
 	size_t lower = left;
 	size_t greater = right - 1;
 
 	size_t i = left;
-	while (i <= greater) <%
-		int cmp = comparison_fn_t(pairs<:i:>.key, pivot);
+	while (i <= greater) {
+		int cmp = comparison_fn_t(pairs[i].key, pivot);
 
-		if (cmp < 0) <%
-			KVpair tmp = pairs<:lower:>;
-			pairs<:lower:> = pairs<:i:>;
-			pairs<:i:> = tmp;
+		if (cmp < 0) {
+			KVpair tmp = pairs[lower];
+			pairs[lower] = pairs[i];
+			pairs[i] = tmp;
 
 			lower++;
 			i++;
-		%> else if (cmp > 0) <%
-			KVpair tmp = pairs<:greater:>;
-			pairs<:greater:> = pairs<:i:>;
-			pairs<:i:> = tmp;
+		} else if (cmp > 0) {
+			KVpair tmp = pairs[greater];
+			pairs[greater] = pairs[i];
+			pairs[i] = tmp;
 
 			greater--;
-		%> else <%
+		} else {
 			i++;
-		%>
-	%>
+		}
+	}
 
 	bstQuickSort(comparison_fn_t, pairs, left, lower);
 	bstQuickSort(comparison_fn_t, pairs, greater + 1, right);
-%>
+}
 
-static void bstInsertMedians(BST *bst, KVpair *pairs, size_t left, size_t right) <%
+static void bstInsertMedians(BST *bst, KVpair *pairs, size_t left, size_t right) {
 	if (left >= right) return;
 
 	size_t median = left + (right - left) / 2;
-	KVpair medianPair = pairs<:median:>;
+	KVpair medianPair = pairs[median];
 	bstInsert(bst, medianPair.key, medianPair.value);
 
 	bstInsertMedians(bst, pairs, left, median);
 	bstInsertMedians(bst, pairs, median + 1, right);
-%>
+}
 
-static void bstRangeSearchRecursive(int comparison_fn_t(void *, void *), BNode *node, void *keymin, void *keymax, List *result) <%
+static void bstRangeSearchRecursive(int comparison_fn_t(void *, void *), BNode *node, void *keymin, void *keymax, List *result) {
 	if (!node) return;
 
 	int cmpMin = comparison_fn_t(keymin, node->key);
 	int cmpMax = comparison_fn_t(keymax, node->key);
 
-	if (cmpMin <= 0) <%
+	if (cmpMin <= 0) {
 		bstRangeSearchRecursive(comparison_fn_t, node->left, keymin, keymax, result);
-	%>
-	if (cmpMin <= 0 && cmpMax >= 0) <%
+	}
+	if (cmpMin <= 0 && cmpMax >= 0) {
 		listInsertLast(result, node->value);
-	%>
-	if (cmpMax > 0) <%
+	}
+	if (cmpMax > 0) {
 		bstRangeSearchRecursive(comparison_fn_t, node->right, keymin, keymax, result);
-	%>
-%>
+	}
+}
 
-static void depthRecursive(BNode *node, size_t depth, size_t *sum, size_t *n) <%
+static void depthRecursive(BNode *node, size_t depth, size_t *sum, size_t *n) {
 	if (!node) return;
 	*sum += depth;
 	(*n)++;
 	depthRecursive(node->left, depth + 1, sum, n);
 	depthRecursive(node->right, depth + 1, sum, n);
-%>
+}
--- c/univ/sda-project-2/src/PointDct/PointDctBST.c
+++ c/univ/sda-project-2/src/PointDct/PointDctBST.c
@@ -1,22 +1,22 @@
-%:include <stdlib.h>
-%:include <stdint.h>
-%:include <math.h>
-%:include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
 
-%:include "PointDct.h"
-%:include "List.h"
-%:include "Point.h"
-%:include "BST.h"
+#include "PointDct.h"
+#include "List.h"
+#include "Point.h"
+#include "BST.h"
 
-typedef struct PointValue_t <%
+typedef struct PointValue_t {
 	Point *point;
 	void *value;
-%> PointValue;
+} PointValue;
 
-struct PointDct_t <%
+struct PointDct_t {
 	BST *bst;
 	double xmin, xmax, ymin, ymax;
-%>;
+};
 
 /* ======= Prototypes of static functions ======= */
 
@@ -41,93 +41,95 @@ static uint64_t pointToZ(Point *p, double xmin, double xmax, double ymin, double
 
 /* ======= Public functions ======= */
 
-PointDct *pdctCreate(List *lpoints, List *Lvalues) <%
+PointDct *pdctCreate(List *lpoints, List *Lvalues) {
 	if (!lpoints || !Lvalues || lpoints->size == 0) return NULL;
 
 	PointDct *pd = malloc(sizeof(PointDct));
-	if (!pd) <%
+	if (!pd) {
 		fprintf(stderr, "pdctCreate: allocation error\n");
 		exit(1);
-	%>
+	}
 
 	LNode *n = lpoints->head;
 	pd->xmin = ptGetx((Point *)n->value);
 	pd->xmax = pd->xmin;
 	pd->ymin = ptGety((Point *)n->value);
 	pd->ymax = pd->ymin;
-	for (n = n->next; n; n = n->next) <%
+	for (n = n->next; n; n = n->next) {
 		double x = ptGetx((Point *)n->value);
 		double y = ptGety((Point *)n->value);
 		if (x < pd->xmin) pd->xmin = x;
 		if (x > pd->xmax) pd->xmax = x;
 		if (y < pd->ymin) pd->ymin = y;
 		if (y > pd->ymax) pd->ymax = y;
-	%>
+	}
 
 	List *zList = listNew();
 	List *pvList = listNew();
-	if (!zList || !pvList) <%
+	if (!zList || !pvList) {
 		fprintf(stderr, "pdctCreate: allocation error\n");
 		exit(1);
-	%>
+	}
 
 	LNode *pointNode = lpoints->head;
 	LNode *valueNode = Lvalues->head;
-	while (pointNode && valueNode) <%
+	while (pointNode && valueNode) {
 		uint64_t z = pointToZ((Point *)pointNode->value, pd->xmin, pd->xmax, pd->ymin, pd->ymax);
 		listInsertLast(zList, (void *)(uintptr_t)z);
 
 		PointValue *pv = malloc(sizeof(PointValue));
-		if (!pv) <%
+		if (!pv) {
 			fprintf(stderr, "pdctCreate: allocation error\n");
 			exit(1);
-		%>
+		}
 		pv->point = pointNode->value;
 		pv->value = valueNode->value;
 		listInsertLast(pvList, pv);
 
 		pointNode = pointNode->next;
 		valueNode = valueNode->next;
-	%>
+	}
 
 	pd->bst = bstOptimalBuild(compareZ, zList, pvList);
 	listFree(zList, false);
 	listFree(pvList, false);
 
 	return pd;
-%>
+}
 
-void pdctFree(PointDct *pd) <%
+void pdctFree(PointDct *pd) {
 	if (!pd) return;
 	bstFree(pd->bst, false, true);
 	free(pd);
-%>
+}
 
-size_t pdctSize(PointDct *pd) <% return bstSize(pd->bst); %>
+size_t pdctSize(PointDct *pd) { return bstSize(pd->bst); }
 
-size_t pdctAverageNodeDepth(PointDct *pd) <% return (size_t)bstAverageNodeDepth(pd->bst); %>
+size_t pdctAverageNodeDepth(PointDct *pd) { return (size_t)bstAverageNodeDepth(pd->bst); }
 
-size_t pdctHeight(PointDct *pd) <% return bstHeight(pd->bst); %>
+size_t pdctHeight(PointDct *pd) { return bstHeight(pd->bst); }
 
-void *pdctExactSearch(PointDct *pd, Point *p) <%
+void *pdctExactSearch(PointDct *pd, Point *p) {
 	if (!pd || !p) return NULL;
 
 	uint64_t z = pointToZ(p, pd->xmin, pd->xmax, pd->ymin, pd->ymax);
 
 	List *candidates = bstRangeSearch(pd->bst, (void *)(uintptr_t)z, (void *)(uintptr_t)z);
+	if (!candidates) return NULL;
+
 	void *result = NULL;
-	for (LNode *node = candidates->head; node; node = node->next) <%
+	for (LNode *node = candidates->head; node; node = node->next) {
 		PointValue *pv = node->value;
-		if (ptCompare(pv->point, p) == 0) <%
+		if (ptCompare(pv->point, p) == 0) {
 			result = pv->value;
 			break;
-		%>
-	%>
+		}
+	}
 	listFree(candidates, false);
 	return result;
-%>
+}
 
-List *pdctBallSearch(PointDct *pd, Point *p, double r) <%
+List *pdctBallSearch(PointDct *pd, Point *p, double r) {
 	if (!pd || !p) return NULL;
 
 	Point *pointA = ptNewFromXY(ptGetx(p) - r, ptGety(p) - r);
@@ -138,71 +140,72 @@ List *pdctBallSearch(PointDct *pd, Point *p, double r) <%
 	ptFree(pointB);
 
 	List *list = bstRangeSearch(pd->bst, (void *)(uintptr_t)zA, (void *)(uintptr_t)zB);
+	if (!list) return NULL;
 
 	double r2 = r * r;
 	LNode *prev = NULL;
 	LNode *node = list->head;
-	while (node) <%
+	while (node) {
 		LNode *next = node->next;
 		PointValue *pointValue = node->value;
 
-		if (ptSqrDistance(p, pointValue->point) > r2) <%
-			if (!prev) <%
+		if (ptSqrDistance(p, pointValue->point) > r2) {
+			if (!prev) {
 				list->head = next;
-			%> else <%
+			} else {
 				prev->next = next;
-			%>
+			}
 
-			if (node == list->last) <%
+			if (node == list->last) {
 				list->last = prev;
-			%>
+			}
 
 			free(node);
 			list->size--;
-		%> else <%
+		} else {
 			node->value = pointValue->value;
 			prev = node;
-		%>
+		}
 
 		node = next;
-	%>
+	}
 
-	if (list->size == 0) <%
+	if (list->size == 0) {
 		list->head = NULL;
 		list->last = NULL;
-	%>
+	}
 
 	return list;
-%>
+}
 
 /* ======= Static functions ======= */
 
-static uint64_t zEncode(uint32_t x, uint32_t y) <%
+static uint64_t zEncode(uint32_t x, uint32_t y) {
 	uint64_t out = 0;
-	for (uint8_t byte = 0; byte < 4; ++byte) <%
+	for (uint8_t byte = 0; byte < 4; ++byte) {
 		out |= interleave8((x >> (byte * 8)) & 0xFF, (y >> (byte * 8)) & 0xFF) << (byte * 16);
-	%>
+	}
 	return out;
-%>
+}
 
-static uint64_t interleave8(uint8_t m, uint8_t n) <%
+static uint64_t interleave8(uint8_t m, uint8_t n) {
 	return (
 		((m * 0x0101010101010101ULL & 0x8040201008040201ULL) * 0x0102040810204081ULL >> 49) & 0x5555
 	) | (
 		((n * 0x0101010101010101ULL & 0x8040201008040201ULL) * 0x0102040810204081ULL >> 48) & 0xAAAA
 	);
-%>
+}
 
-static int compareZ(void *a, void *b) <%
+static int compareZ(void *a, void *b) {
 	uint64_t za = (uint64_t)(uintptr_t)a;
 	uint64_t zb = (uint64_t)(uintptr_t)b;
 	return za < zb ? -1 : za > zb ? 1 : 0;
-%>
+}
 
-static uint64_t pointToZ(Point *p, double xMin, double xMax, double yMin, double yMax) <%
+static uint64_t pointToZ(Point *p, double xMin, double xMax, double yMin, double yMax) {
 	double doubleX = (xMax > xMin) ? (ptGetx(p) - xMin) / (xMax - xMin) * UINT32_MAX : 0;
 	double doubleY = (yMax > yMin) ? (ptGety(p) - yMin) / (yMax - yMin) * UINT32_MAX : 0;
 	uint32_t intX = doubleX <= 0 ? 0 : doubleX >= UINT32_MAX ? UINT32_MAX : (uint32_t)doubleX;
 	uint32_t intY = doubleY <= 0 ? 0 : doubleY >= UINT32_MAX ? UINT32_MAX : (uint32_t)doubleY;
 	return zEncode(intX, intY);
-%>
+}

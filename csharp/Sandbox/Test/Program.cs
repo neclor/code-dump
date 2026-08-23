d1c24a02f8bbd5a0c14f287209d7dd10376cc31b
neclor <neclordev@gmail.com>
2026-08-23 21:30:40 +0200

    a

--- csharp/Sandbox/Test/Program.cs
+++ csharp/Sandbox/Test/Program.cs
@@ -1,25 +1,68 @@
 namespace Test;
 
-public interface IFoo {
-	string Foo() => "interface";
-}
+internal static class Program {
 
-public static class FooExtensions {
-	extension(IFoo foo) {
-		public string Foo() => foo.Foo();
+	private static void Main() {
+
+		June2019.First();
 	}
 }
 
-public class FooA : IFoo { }
 
-public class FooB : FooA, IFoo {
-	string IFoo.Foo() => "B";
-}
+internal static class June2019 {
 
-public static class Program {
-	public static void Main() {
-		FooB foo = new FooB();
+	internal static void First() {
+		Console.WriteLine("June 2019 (1)");
 
-		Console.WriteLine(foo.Foo());
+		Random rng = new();
+		List<int> array = [];
+
+		const int Value = 3;
+
+		for (int e = 0; e < 20; e++) {
+			array.Add(rng.Next(-10, 11));
+		}
+		array.Sort();
+		Console.WriteLine(string.Join(", ", array));
+
+		int i = 0;
+		int j = array.Count - 1;
+		int count = 0;
+
+		while (i < j) {
+			int diff = array[i] + array[j];
+
+			if (diff < Value) {
+				i++;
+
+			} else if (diff > Value) {
+				j--;
+
+			} else {
+				int low = array[i];
+				int high = array[j];
+
+				if (low == high) {
+					int n = j - i + 1;
+					count += n * (n - 1) / 2;
+					break;
+				}
+
+				int lowCount = 0;
+				int highCount = 0;
+
+				for (; array[i] == low; i++) {
+					lowCount++;
+				}
+
+				for (; array[j] == high; j--) {
+					highCount++;
+				}
+
+				count += lowCount * highCount;
+			}
+		}
+
+		Console.WriteLine(count);
 	}
 }
